fdb54769191d050a13c2c9ee648a25d7cf8613ff
Michael <56491958+menestrel-mkmb@users.noreply.github.com>
2024-09-14 19:14:36 -0300

    feat(bot): change /hello to ensure template verification;

--- src/modules/app.service.ts
+++ src/modules/app.service.ts
@@ -8,6 +8,6 @@ export class AppService {
     description: 'Hello from a nest application',
   })
   public async onPing(@Context() [interaction]: SlashCommandContext) {
-    return interaction.reply({ content: 'from nest.js via necord!' });
+    return interaction.reply({ content: 'from nest.js via template with necord!' });
   }
 }
