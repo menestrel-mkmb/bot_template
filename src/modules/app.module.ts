a75f715e8aed98e12e7aa9ed737e35d5f7ab6464
Michael <56491958+menestrel-mkmb@users.noreply.github.com>
2024-09-14 19:14:02 -0300

    feat(bot): Module(?) with basic info structure;

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,48 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import {
+  // BitFieldResolvable,
+  // GatewayIntentsString,
+  // IntentsBitField,
+  Partials,
+} from 'discord.js';
+import { NecordModule } from 'necord';
+
+import { AppService } from './app.service';
+import { AppUpdate } from './app.update';
 
 import { CommonModule } from './common';
 import { PassengerModule } from './passenger/passenger.module';
 
+
 @Module({
     imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env',
+          isGlobal: true
+        }),
         CommonModule,
-        PassengerModule
-    ]
+        PassengerModule,
+        NecordModule.forRoot({
+            token: process.env.DISCORD_BOT_TOKEN || '',
+            intents: ['Guilds', 'GuildMessages', 'DirectMessages'],
+            // intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<
+            //   GatewayIntentsString,
+            //   number
+            // >,
+            partials: [
+              Partials.Channel,
+              Partials.GuildMember,
+              Partials.GuildScheduledEvent,
+              Partials.Message,
+              Partials.Reaction,
+              Partials.User,
+              Partials.ThreadMember,
+            ],
+          }),
+    ],
+    providers: [AppService, AppUpdate],
+    exports: [AppService],
 })
 export class ApplicationModule {}
