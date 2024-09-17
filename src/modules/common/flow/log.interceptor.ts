78afe68a4a04b6b393d4acdd5d35d9864860a9b3
Michael <56491958+menestrel-mkmb@users.noreply.github.com>
2024-09-17 16:08:57 -0300

    refactor(log): deprecated throwError removed;

--- src/modules/common/flow/log.interceptor.ts
+++ src/modules/common/flow/log.interceptor.ts
@@ -1,6 +1,6 @@
 import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { LoggerService } from '../provider';
@@ -20,15 +20,16 @@ export class LogInterceptor implements NestInterceptor {
         return next.handle().pipe(
             map((data: FastifyReply) => {
                 const responseStatus = (request.method === 'POST') ? HttpStatus.CREATED : HttpStatus.OK;
+                // TODO: undefined undefined undefined undefined
                 this.logger.info(`${this.getTimeDelta(startTime)}ms ${request.ip} ${responseStatus} ${request.method} ${this.getUrl(request)}`);
                 return data;
             }),
-            catchError((err: unknown) => {
+            catchError(async (err: unknown) => {
                 // Log fomat inspired by the Squid docs
                 // See https://docs.trafficserver.apache.org/en/6.1.x/admin-guide/monitoring/logging/log-formats.en.html
                 const status = this.hasStatus(err) ? err.status : 'XXX';
                 this.logger.error(`${this.getTimeDelta(startTime)}ms ${request.ip} ${status} ${request.method} ${this.getUrl(request)}`);
-                return throwError(err);
+                return Promise.reject(err);
             })
         );
     }
