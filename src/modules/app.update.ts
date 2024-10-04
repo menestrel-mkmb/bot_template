0c8b134398ec51e8ef6866e4cc7f9c22b4c66342
Michael <56491958+menestrel-mkmb@users.noreply.github.com>
2024-10-04 19:34:58 -0300

    fix(necord): avoid dupe singleton constructor;

--- src/modules/app.update.ts
+++ src/modules/app.update.ts
@@ -1,24 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Client, Partials } from 'discord.js';
 import { Context, On, Once, ContextOf } from 'necord';
 
 @Injectable()
 export class AppUpdate {
   private readonly logger = new Logger(AppUpdate.name);
-  private readonly client: Client = new Client({
-    intents: ['Guilds', 'GuildMessages', 'DirectMessages'],
-    partials: [Partials.Channel, Partials.GuildMember, Partials.Message, Partials.User],
-  });
 
   @Once('ready')
-  public async onReady(@Context() [c]: ContextOf<'ready'>) {
-    await this.client.login(process.env.BOT_TOKEN)
-      .then(() => {
-        this.logger.log(`Bot logged in as ${c.user.username}`);
-      })
-      .catch((err) => {
-        this.logger.error(`Log com undefined request: ${err}`);
-      });
+  public onReady(@Context() [c]: ContextOf<'ready'>) {
+    this.logger.log(`Logged in as ${c.user.tag}!`);
   }
 
   @On('warn')
